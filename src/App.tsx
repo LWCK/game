229df940ca61ea8be6b236e9fc5d1c0567cbae87
LWCK <christophe.kromer@ylwane.com>
2022-08-24 17:08:01 -1000

    step 2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
 
   const [gameList, setGameList] = React.useState<any>({
     id: 1,
-    rule: "Pour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettre.",
+    rule: "Pour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettrePour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettrePour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettrePour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettrePour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettrePour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettre.",
     step: 1
   }
   )
@@ -52,16 +52,35 @@ const App = () => {
           </Row>
         }
         {rule &&
-          <Row className='only-landscape bg text-bg intro-rules'>
-            <Col className='' xs={3}>
-              <Image className='char-img' src="/src/assets/images/char.png" alt="" />
-            </Col>
-            <Col xs={9} className='text-end py-3 px-3'>
-              <h3 className='text-start'>Règle du jeu</h3>
-              <p className='text text-start'>{gameList.rule}</p>
-              <Button onClick={() => imgGame()} variant='' className='btn-next bounce'><Image className='img-in-btn' src="/src/assets/images/sword.png" alt="" /></Button>
-            </Col>
-          </Row>
+          <>
+
+            <div className="_test0 text-end">
+              <div className='_test1'>
+                <div >
+                  <Image className='char-img' src="/src/assets/images/char.png" alt="" />
+                </div>
+              </div>
+
+              <div className='_test2'>
+                <div className='_test3'>
+                  <h3 className='text-start'>Règle du jeu</h3>
+                  <p className='text text-start'>{gameList.rule}</p>
+                </div>
+                <Button onClick={() => imgGame()} variant='' className='btn-next bounce'><Image className='img-in-btn' src="/src/assets/images/sword.png" alt="" /></Button>
+              </div>
+            </div>
+
+            {/* <Row className='only-landscape bg text-bg intro-rules'>
+              <Col className='' xs={3}>
+                <Image className='char-img' src="/src/assets/images/char.png" alt="" />
+              </Col>
+              <Col xs={9} className='text-end py-3 px-3'>
+                <h3 className='text-start'>Règle du jeu</h3>
+                <p className='text text-start'>{gameList.rule}</p>
+                <Button onClick={() => imgGame()} variant='' className='btn-next bounce'><Image className='img-in-btn' src="/src/assets/images/sword.png" alt="" /></Button>
+              </Col>
+            </Row> */}
+          </>
         }
       </Container>
 
