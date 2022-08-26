4b11103a0be823c1b75c0efbdd7092df41f9e0fc
LWCK <christophe.kromer@ylwane.com>
2022-08-26 09:11:43 -1000

    slug router step clean

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
+import Step from './components/Step/Step';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      {/* <Route path="/gameId" element={<App />} /> */}
+      <Route path='/jeu/:step' element={<Step />} />
+    </Routes>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 )
+
