d03641771f7c0c2a361756b6f5d4680cb3cf1778
LWCK <christophe.kromer@ylwane.com>
2022-08-25 15:22:24 -1000

    road to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,143 @@
 import './App.css'
 import { Button, Col, Container, Image, Row } from 'react-bootstrap'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const App = () => {
 
-
-  const [gameList, setGameList] = React.useState<any>({
-    id: 1,
-    rule: "Pour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettrePour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettrePour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettrePour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettrePour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettrePour commencer, déchiffrez le nom du rayon où vous rendre. Chaque élément correspond à une lettre.",
-    step: 1
-  }
-  )
-
-  const [begin, setBegin] = React.useState<boolean>(true)
-  const [rule, setRule] = React.useState<boolean>(false)
-  const [game, setGame] = React.useState<boolean>(false)
-
-  const startGame = () => {
-    setBegin(false)
-    setRule(true)
-  }
-
-  const imgGame = () => {
-    setGame(true)
-    setRule(false)
-  }
-
-  return (
-    <>
-      <Container fluid>
-        <Row>
-          <Col xs={12} className="gif-orientation text-center">
-            <Image src="/src/assets/images/turnscreen.gif" alt="" />
-            <h2 className="title">
-              Pour commencer, <br />
-              veuillez pivoter le téléphone
-            </h2>
-          </Col>
-        </Row>
-        {begin &&
-          <Row className='only-landscape main-story'>
-            <Col className="offset-6 col-6 main">
-              <span className="text-center main-bg bg px-5 py-3">
-                <h2 className="title">
-                  Le trésor du legéndaire <br />
-                  pirate la buse
-                </h2>
-                <Button onClick={() => startGame()} variant='' className="btn text px-2 bounce">Commencer le jeu</Button>
-              </span>
-            </Col>
-          </Row>
-        }
-        {rule &&
-          <>
-
-            <div className="_test0 text-end">
-              <div className='_test1'>
-                <div >
-                  <Image className='char-img' src="/src/assets/images/char.png" alt="" />
-                </div>
-              </div>
-
-              <div className='_test2'>
-                <div className='_test3'>
-                  <h3 className='text-start'>Règle du jeu</h3>
-                  <p className='text text-start'>{gameList.rule}</p>
-                </div>
-                <Button onClick={() => imgGame()} variant='' className='btn-next bounce'><Image className='img-in-btn' src="/src/assets/images/sword.png" alt="" /></Button>
-              </div>
-            </div>
-
-            {/* <Row className='only-landscape bg text-bg intro-rules'>
-              <Col className='' xs={3}>
-                <Image className='char-img' src="/src/assets/images/char.png" alt="" />
-              </Col>
-              <Col xs={9} className='text-end py-3 px-3'>
-                <h3 className='text-start'>Règle du jeu</h3>
-                <p className='text text-start'>{gameList.rule}</p>
-                <Button onClick={() => imgGame()} variant='' className='btn-next bounce'><Image className='img-in-btn' src="/src/assets/images/sword.png" alt="" /></Button>
-              </Col>
-            </Row> */}
-          </>
-        }
-      </Container>
-
-      {
-        game &&
-        <Container>
-          <Row>
-            <Col className='text-center'>
-              <Image className='only-landscape game-img' src='/src/assets/images/game1.png' />
-            </Col>
-          </Row>
-        </Container>
-
-      }
+	const [gameList, setGameList] = React.useState<any>('')
+	useEffect(() => {
+		fetch("http://192.168.1.250:1337/api/pages?populate=*")
+			.then(response => response.json())
+			.then(result => setGameList(result))
+			.catch(error => console.log('error', error));
+	}, []
+	)
 
 
-    </>
-  )
+	const [rule, setRule] = React.useState<any>('')
+	useEffect(() => {
+		fetch("http://192.168.1.250:1337/api/rule")
+			.then(response => response.json())
+			.then(result => setRule(result))
+			.catch(error => console.log('error', error));
+	}, []
+	)
+
+	// console.log(gameList);
+	console.log(rule && rule.data.attributes.text);
+
+	const [step1, setStep1] = React.useState<boolean>(true)
+	const [step2, setStep2] = React.useState<boolean>(false)
+	const [step21, setStep21] = React.useState<boolean>(false)
+	const [step3, setStep3] = React.useState<boolean>(false)
+
+	const startGame = () => {
+		setStep1(false)
+		setStep2(true)
+	}
+
+	const showIntro = () => {
+		setStep21(true)
+		setStep2(false)
+	}
+
+
+	const imgGame = () => {
+		setStep2(false)
+		setStep21(false)
+		setStep3(true)
+	}
+
+	console.log(gameList.intro);
+
+
+	return (
+		<>
+
+			<Container>
+				<Row>
+					<Col xs={12} className="gif-orientation text-center">
+						<Image src="/src/assets/images/turnscreen.gif" alt="" />
+						<h2 className="title">
+							Pour commencer, <br />
+							veuillez pivoter le téléphone
+						</h2>
+					</Col>
+				</Row>
+			</Container>
+
+			{step1 &&
+
+				<Container className='vh-100 main only-portrait'>
+					<Row className="bg p-4">
+						<Col xs={12} className='text-center '>
+							<h2 className="title">
+								Le trésor du legéndaire <br />
+								pirate la buse
+							</h2>
+							<Button onClick={() => startGame()} variant='' className="btn text">Commencer</Button>
+						</Col>
+					</Row>
+				</Container>
+			}
+			{
+				step2 &&
+				<>
+					<Container className='only-portrait main p-5 '>
+						<Row>
+							<Image className='char-img-2' src="/src/assets/images/char2.png" alt="" />
+							<div className='text-center char-text bg title'>
+
+
+								<h2>Salut moussaillon !!</h2>
+								<Button onClick={() => showIntro()} variant='' className='text-end'>
+									Rappel des règles
+								</Button>
+								{/* <Button onClick={() => imgGame()} variant='' className='text-end'>
+									jeu
+								</Button> */}
+
+							</div>
+						</Row>
+					</Container>
+				</>
+			}
+
+			{step21 &&
+				<>
+
+					<Container fluid className='vh-100 main only-portrait bg'>
+						<Row className="p-3">
+							<Col xs={12} className='text text-center'>
+								<p className='text-justify text'>
+									{rule && rule.data.attributes.text}
+								</p>
+								<Button onClick={() => imgGame()} variant='' className="btn text">Commencer
+
+									{/* //step */}
+
+								</Button>
+							</Col>
+						</Row>
+					</Container>
+
+				</>
+			}
+
+			{
+				step3 &&
+				<Container className='only-portrait main bg'>
+					<Row>
+						<Col className='text-center'>
+							<h2>Règles du Jeu</h2>
+							<p>{gameList.rules}</p>
+							<Image className='game-img' src='/src/assets/images/game1.png' />
+						</Col>
+					</Row>
+				</Container>
+			}
+		</>
+	)
 }
 
 
